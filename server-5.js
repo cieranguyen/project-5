a24ff0569ce645fb362f45968214bd35095abdbe
Ciera Nguyen <cieran@uoregon.edu>
2020-06-02 16:20:39 -0700

    Complete server-7.js

--- server-5.js
+++ server-5.js
@@ -1,28 +1,47 @@
 //jshint esversion: 6
-
-// simple Express.js RESTful API
+// Adds endpoint for HTTP Post request
 
 // initialize
+
 const
   port = 8888,
   bodyParser = require('body-parser'),
   express = require('express'),
   app = express();
 
+/*
 app.use(express.urlencoded({
   extended: true
 }));
-
 app.use(express.json());
+*/
 
 //if the client directory contains an "index.html" web page
 //  it will be displayed as the default document
 app.use(express.static(__dirname + "/client"));
 
+// Parse data from incoming Post requests
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
 // enable CORS
 app.use((req, res, next) => {
   res.append('Access-Control-Allow-Origin', '*');
   next();
+});
+
+//Simple route handle to test a Post request endpoint
+//test it with Postwoman.io or Postman app
+app.post('/addnum', (req, res) => {
+ const telnum = {
+   name: req.body.name,
+   number: req.body.number
+ }
+ return res.status(201).send({
+   success: 'true',
+   message: 'new number added',
+   telnum
+ })
 });
 
 // /hello/ route, an HTTP GET request
@@ -35,16 +54,4 @@ app.get('/hello/:name?', (req, res) =>
 // start server
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
-});
-
-app.post('/addnum', (req, res) => {
-const telnum = {
-  name: req.body.name,
-  number: req.body,number
-}
-return res.status(201).send({
-  success: 'true',
-  message: 'new number added',
-  telnum
-})
 });
--- server-6.js
+++ server-6.js
@@ -10,22 +10,31 @@ const db = [
 ];
 export default tel_book;
 
+//jshint esversion: 6
+//Adds Post data to db in server
+
 // initialize
+
 const
   port = 8888,
   bodyParser = require('body-parser'),
   express = require('express'),
-  app = express();
-
-app.use(express.urlencoded({
-  extended: true
-}));
-
-app.use(express.json());
+  app = express(),
+  db =  [
+     {
+       "id": 1,
+       "name": "Argus Filch",
+       "number": "800-Hog-Wart"
+     }
+  ];
 
 //if the client directory contains an "index.html" web page
 //  it will be displayed as the default document
 app.use(express.static(__dirname + "/client"));
+
+// Parse data from incoming Post requests
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 // enable CORS
 app.use((req, res, next) => {
@@ -33,41 +42,32 @@ app.use((req, res, next) => {
   next();
 });
 
-// /hello/ route, an HTTP GET request
-app.get('/hello/:name?', (req, res) =>
-  res.json({
-    message: `Hello ${req.params.name || 'world'}!`
+// endpoint to Get all tel numbers
+app.get('/telnums', (req, res) => {
+  res.status(200).send({
+    success: 'true',
+    message: 'tel numbers retrieved successfully',
+    tel_nums: db
   })
-);
+});
+
+//Post request endpoint
+//test it with Postman app
+app.post('/addnum', (req, res) => {
+  const telnum = {
+    id: db.length + 1,
+    name: req.body.name,
+    number: req.body.number
+  }
+ db.push(telnum);  //add record to local db
+ return res.status(201).send({
+   success: 'true',
+   message: 'Telephone Number added successfully',
+   telnum
+ })
+});
 
 // start server
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
-});
-
-app.post('/addnum', (req, res) => {
-const telnum = {
-  name: req.body.name,
-  number: req.body,number
-}
-return res.status(201).send({
-  success: 'true',
-  message: 'new number added',
-  telnum
-})
-});
-
-// endpoint to Get all tel numbers
-app.post('/addnum', (req, res) => {
-const telnum = {
-id: db.length + 1,
-name: req.body.name,
-number: req.body.number
-}
-db.push(telnum); //add record to local db
-return res.status(201).send({
-success: 'true',
-message: 'Telephone Number added successfully',
-telnum
-})
 });
